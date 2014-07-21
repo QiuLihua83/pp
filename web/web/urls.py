11b519bae9c5afb83ea60ae790f1e21ed8aaf3da
qiulihua <qiulihua83@gmail.com>
2014-07-21 21:58:08 +0800

    加入主页根目录

--- web/web/urls.py
+++ web/web/urls.py
@@ -11,7 +11,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'web.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$',views.index),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^index/$',views.index),
 )
