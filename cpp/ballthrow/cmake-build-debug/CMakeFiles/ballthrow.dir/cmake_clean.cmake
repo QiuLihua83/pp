32418861fb8c725731656d2ad2165f8c734fe373
qiulihua83 <qiulihua83@qq.com>
2017-03-18 18:48:48 +0800

    new mod

---
+++ cpp/ballthrow/cmake-build-debug/CMakeFiles/ballthrow.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ballthrow.dir/main.cpp.o"
+  "ballthrow.pdb"
+  "ballthrow"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ballthrow.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
